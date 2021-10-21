9e0ae7a24c82fbb76b280fe1a24354041291ee2b
Shaun Pour <scpour@gmail.com>
2021-10-21 09:29:58 -0400

    1.0

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let countDate = new Date("Jan 1, 2022 00:00:00").getTime();
+
+let x = setInterval(function() {
+    let currentDateTime = new Date().getTime();
+    let distance = countDate - currentDateTime;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.querySelector(".days").innerHTML = days + "d";
+    document.querySelector(".hours").innerHTML = hours + "h";
+    document.querySelector(".minutes").innerHTML = minutes + "m";
+    document.querySelector(".seconds").innerHTML = seconds + "s";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.querySelector(".countdown").innerHTML = "TIMER EXPIRED";
+    }
+})
